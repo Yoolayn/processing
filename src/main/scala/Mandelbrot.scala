df845790a872f0ff1191d5c846897055114c83cc
Yoolayn <jaroszynskijulian@gmail.com>
2024-02-29 01:43:21 +0100

    feat: iterative style -> functional style

--- src/main/scala/Mandelbrot.scala
+++ src/main/scala/Mandelbrot.scala
@@ -2,6 +2,10 @@ import processing.core._
 import scala.math
 
 class Mandelbrot extends PApplet {
+
+    val maxIterations: Int = 100
+    val max: Float = 4.0
+
     override def settings(): Unit = {
         size(1920, 1080)
     }
@@ -11,65 +15,73 @@ class Mandelbrot extends PApplet {
         background(255)
         loadPixels()
 
-        var w: Float = 4
-        var h: Float = (w * height) / width
+        val w: Float = 4
+        val h: Float = (w * height) / width
 
-        var xmin: Float = -w/2
-        var ymin: Float = -h/2
+        val xmin: Float = -w/2
+        val ymin: Float = -h/2
 
-        var maxIterations = 100
+        val xmax = xmin + w
+        val ymax = ymin + h
 
-        var xmax = xmin + w
-        var ymax = ymin + h
+        val dx = (xmax - xmin) / width
+        val dy = (ymax - ymin) / height
 
-        var dx = (xmax - xmin) / width
-        var dy = (ymax - ymin) / height
-
-        var y: Float = ymin
-
-        for (j: Int <- 0 to height - 1) {
-            var x = xmin
-
-            for (i: Int <- 0 to width - 1) {
-                var a = x
-                var b = y
-                var n = 0
-                var max: Float = 4.0
-                var absOld: Float = 0.0
-                var convergeNumber: Float = maxIterations
-
-                var break: Boolean = false
-                while (n < maxIterations && !break) {
-                    var aa: Float = a * a
-                    var bb: Float = b * b
-                    var abs: Float = math.sqrt(aa + bb).toFloat
-
-                    if (abs > max) {
-                        var diffToLast: Float = (abs - absOld)
-                        var diffToMax: Float = (max - absOld)
-                        convergeNumber = n + diffToMax / diffToLast
-                        break = true
-                    } else {
-                        var twoab: Float = (2.0 * a * b).toFloat
-                        a = aa - bb + x
-                        b = twoab + y
-                        n += 1
-                        absOld = abs
-                    }
-                }
-
-                if (n == maxIterations) {
-                    pixels(i + j * width) = color(0)
-                } else {
-                    var norm: Float = PApplet.map(convergeNumber, 0, maxIterations, 0, 1)
-                    pixels(i + j * width) = color(PApplet.map(math.sqrt(norm).toFloat, 0, 1, 0, 255))
-                }
-                x += dx
+        def fillPixels(): Unit = {
+            @annotation.tailrec
+            def helper(y: Float, iter: Int): Unit = {
+                if (iter > height - 1) return
+                forLoop(xmin, y, iter, dx)
+                helper(y + dy, iter + 1)
             }
-            y += dy
+            helper(ymin, 0)
         }
+
+        fillPixels()
         updatePixels()
     }
+
+    def forLoop(x: Float, y: Float, outerIter: Int, dx: Float): Unit = {
+        @annotation.tailrec
+        def helper(iter: Int, x: Float): Unit = {
+            if (iter > width - 1) return
+            val (n, convergeNumber) = while_loop(x, y)
+
+            if (n == maxIterations) {
+                pixels(iter + outerIter * width) = color(0)
+            } else {
+                val norm: Float = PApplet.map(convergeNumber, 0, maxIterations, 0, 1)
+                pixels(iter + outerIter * width) = color(PApplet.map(math.sqrt(norm).toFloat, 0, 1, 0, 255))
+            }
+            helper(iter + 1, x + dx)
+        }
+        helper(0, x)
+    }
+
+    def while_loop(x: Float, y: Float): (Int, Float) = {
+        @annotation.tailrec
+        def helper(a: Float, b: Float, n: Int, absOld: Float): (Int, Float) = {
+            val aa: Float = a * a
+            val bb: Float = b * b
+            val abs: Float = math.sqrt(aa + bb).toFloat
+
+            if (abs > max) {
+                val diffToLast: Float = (abs - absOld)
+                val diffToMax: Float = (max - absOld)
+                val newConvNum = n + diffToMax / diffToLast
+                (n, newConvNum)
+            } else if (n < maxIterations) {
+                val twoab: Float = (2.0 * a * b).toFloat
+                val newA = aa - bb + x
+                val newB = twoab + y
+                helper(newA, newB, n + 1, abs)
+            } else {
+                (n, maxIterations)
+            }
+        }
+        helper(x, y, 0, 0.0)
+    }
+
 }
 
 @main
